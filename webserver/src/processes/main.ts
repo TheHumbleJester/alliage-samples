3a3dee47a8151ea503b9ed351c58dd9e26cfb446
The Humble Jester <the.humble.jester@gmail.com>
2020-08-14 13:01:39 +0200

    feat: setup webserver

--- webserver/src/processes/main.ts
+++ webserver/src/processes/main.ts
@@ -1,3 +1,5 @@
+import express, { Request, Response } from "express";
+
 import { AbstractProcess } from "alliage-process-manager/process";
 import { Service } from "alliage-service-loader/decorators";
 
@@ -8,7 +10,14 @@ export default class MainProcess extends AbstractProcess {
   }
 
   async execute() {
-    process.stdout.write("Hello Alliage !\n");
-    return true;
+    const app = express();
+
+    app.get("/", (_req: Request, res: Response) => {
+      res.send("Hello world !");
+    });
+
+    app.listen(8080);
+
+    return await this.waitToBeShutdown();
   }
 }
