00df7de9f0a3b030e19ada0aa204e9a29129fc86
The Humble Jester <the.humble.jester@gmail.com>
2020-08-31 10:19:10 +0200

    feat: use a dedicated config file for the web server module

--- webserver/modules/web-server/index.ts
+++ webserver/modules/web-server/index.ts
@@ -1,15 +1,34 @@
-import { AbstractLifeCycleAwareModule } from 'alliage-lifecycle/module'
-import { ServiceContainer } from 'alliage-di/service-container'
-import { parameter, allInstancesOf, Constructor } from 'alliage-di/dependencies';
+import { AbstractLifeCycleAwareModule } from "alliage-lifecycle/module";
+import { ServiceContainer } from "alliage-di/service-container";
+import { parameter, allInstancesOf, Constructor } from "alliage-di/dependencies";
+import { CONFIG_EVENTS } from "alliage-config-loader/events";
+import { loadConfig } from "alliage-config-loader/helpers";
+import { validate } from "alliage-config-loader/validators/json-schema";
 
 import { WebProcess } from './processes/web';
 import { AbstractController } from './controllers/abstract-controller';
 
 export = class WebserverModule extends AbstractLifeCycleAwareModule {
+  getEventHandlers() {
+    return {
+      [CONFIG_EVENTS.LOAD]: loadConfig(
+        "webserver",
+        validate({
+          type: "object",
+          properties: {
+            port: {
+              type: "number",
+            },
+          },
+        })
+      ),
+    };
+  }
+
   registerServices(serviceContainer: ServiceContainer) {
-    serviceContainer.registerService('web_process', WebProcess, [
-      parameter("parameters.webserver.port"),
+    serviceContainer.registerService("web_process", WebProcess, [
+      parameter("webserver.port"),
       allInstancesOf(AbstractController as Constructor),
     ]);
   }
-}
+};
